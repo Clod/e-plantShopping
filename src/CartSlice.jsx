e7dc15160d59f78e61a33ced6ce15df2d17cf81c
Clod <claudio.grasso@yahoo.com>
2025-02-16 17:10:56 -0300

    All assignment tasks completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,34 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      /*
+        action.payload =
+        {
+          name: "Echinacea",
+          image: "https://cdn.pixabay.com/photo/2014/12/05/03/53/echinacea-557477_1280.jpg",
+          description: "Boosts immune system, helps fight colds.",
+          cost: "$16"
+        }
+      */
+      console.log(action.payload);
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      // action.payload = name of the item to be removed
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
